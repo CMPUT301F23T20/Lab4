3f7a794da63fb912799d04757f3fd4abb12192e0
Isaac Joffe <isaac11joffe@gmail.com>
2023-10-04 19:11:47 -0600

    adding string field

--- Lab 4/app/src/main/java/com/example/lab4/Shape.java
+++ Lab 4/app/src/main/java/com/example/lab4/Shape.java
@@ -3,6 +3,7 @@ package com.example.lab4;
 public abstract class Shape{
     private int x;
     private int y;
+    private String colour = "Periwinkle";
 
     public int getX() {
         return x;
